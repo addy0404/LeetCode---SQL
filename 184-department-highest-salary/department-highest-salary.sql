873c9ee4362511533db56e0fd3ecd9806e8e2eb7
Aditya Chawla <54615209+addy0404@users.noreply.github.com>
2024-07-24 22:04:31 -0500

    Time: 750 ms (39.45%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,21 +1,23 @@
 # Write your MySQL query statement below
--- select dept.name as Department, 
---         emp.name as Employee,
---         max(emp.salary) as Salary
--- FROM Department as dept
--- LEFT JOIN Employee as emp
--- ON emp.departmentId = dept.id
--- group by emp.departmentId
-with tmp_table as (select name, departmentId, salary
-from 
-(select name, departmentId, salary, rank() over (partition by departmentId order by salary desc) as rn 
+-- with tmp_table as (select name, departmentId, salary
+-- from 
+-- (select name, departmentId, salary, rank() over (partition by departmentId order by salary desc) as rn 
+-- from Employee
+-- ) as rn_emp
+-- where rn = 1)
+-- select dept.name as Department, tmp_table.name as Employee, tmp_table.salary as Salary
+-- from Department as dept
+-- RIGHT JOIN tmp_table
+-- ON tmp_table.departmentId = dept.id
+
+select dept.name as Department, emp.name as Employee, emp.salary as Salary
+from Employee as emp
+LEFT JOIN Department as dept
+ON dept.id = emp.departmentId
+where (dept.id,emp.salary) 
+IN (select departmentId, MAX(salary)
 from Employee
-) as rn_emp
-where rn = 1)
-select dept.name as Department, tmp_table.name as Employee, tmp_table.salary as Salary
-from Department as dept
-RIGHT JOIN tmp_table
-ON tmp_table.departmentId = dept.id
+group by 1)
 
 
 
