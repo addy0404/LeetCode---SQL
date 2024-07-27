dae998de8b28eebbfd0d7e7d0d52fd2f272742d8
Aditya Chawla <54615209+addy0404@users.noreply.github.com>
2024-07-27 01:18:20 -0500

    Time: 535 ms (86.71%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -5,7 +5,7 @@ count(trans_date) as trans_count,
 sum(case when state="approved" then 1 else 0 end) as approved_count,
 sum(amount) as trans_total_amount, 
 sum(case when state="approved" then amount else 0 end) as approved_total_amount
-FROM Transactions
+FROM Transactions 
 group by 1,2
 
 
