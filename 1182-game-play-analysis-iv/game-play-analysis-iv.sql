c01824214c33e74b4ff0324d0bb7d534498c2de4
Aditya Chawla <54615209+addy0404@users.noreply.github.com>
2024-07-27 22:00:49 -0500

    Time: 987 ms (18.53%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -8,7 +8,7 @@ WITH FirstLogins AS (
     GROUP BY player_id
 ),
 ConsecutiveLogins AS (
-    SELECT a.player_id, a.event_date, f.first_login
+    SELECT a.player_id
     FROM Activity a
     JOIN FirstLogins f ON a.player_id = f.player_id
     WHERE a.event_date = date_add(f.first_login, interval 1 day)
