fcb93fdeca47efe9fb2d76ff4be8ba3113639249
Aditya Chawla <54615209+addy0404@users.noreply.github.com>
2024-08-01 01:57:54 -0500

    Time: 339 ms (53.93%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -5,13 +5,12 @@ with day_amt as (
 ),
 smmry as (
     select visited_on, 
-        sum(amount) over w as amount,
-        avg(amount) over w as average_amount,
-        row_number() over w as row_no
+        sum(amount) over (order by visited_on rows between 6 preceding and current row) as amount,
+        avg(amount) over (order by visited_on rows between 6 preceding and current row) as average_amount,
+        row_number() over (order by visited_on) as row_no
     from day_amt
-    window w as (order by visited_on rows between 6 preceding and current row)
+    
 )
-select visited_on, amount, round(average_amount, 2) as average_amount
-from smmry
+select visited_on, amount, round(average_amount,2) as average_amount from smmry
 where row_no>=7
 order by visited_on
